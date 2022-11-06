1843e3b86bb31c74eabf28e6703d85f6833c2683
KarinaFomina04 <karinafomina.19970804@gmail.com>
2022-11-07 00:15:42 +0100

    Add pagination

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,45 +2,71 @@ import React from "react";
 import style from './App.modules.scss'
 import Collection from '../Collection';
 
+const cats = [
+    { "name": "All" },
+    { "name": "Sea" },
+    { "name": "Mountains" },
+    { "name": "Architecture" },
+    { "name": "Cities" }
+];
 
-function App() {
-    const [searchValue, setsearchValue] = React.useState([]);
+const App: React.FC = () => {
+    const [categoryId, setCategoryId] = React.useState(0);
+    const [isLouding, setIsLouding] = React.useState(true);
+    const [page, setPage] = React.useState(1);
+    const [searchValue, setSearchValue] = React.useState('');
     const [collections, setCollections] = React.useState([]);
     React.useEffect(() => {
-        fetch('https://6366ecd1f5f549f052ce631b.mockapi.io/photos')
+        setIsLouding(true);
+
+        const category = categoryId ? `category=${categoryId}` : '';
+
+        fetch(`https://6366ecd1f5f549f052ce631b.mockapi.io/photos?page=${page}&limit=3&${category}`,
+        )
             .then((res) => res.json())
             .then((json) => {
                 setCollections(json);
             })
             .catch(err => {
                 console.warn(err);
-                alert('Ошибка при получение данных');
-            });
-    }, []);
+                alert('Error while getting data.');
+            })
+            .finally(() => setIsLouding(false));
+    }, [categoryId, page]);
 
     return (
         <div className={style.App}>
-            <h1>Моя коллекция фотографий</h1>
+            <h1>My photos</h1>
             <div className={style.top}>
                 <ul className={style.tags}>
-                    <li className={style.active}>Все</li>
-                    <li>Горы</li>
-                    <li>Море</li>
-                    <li>Архитектура</li>
-                    <li>Города</li>
+                    {cats.map((obj, i) => (
+                        <li
+                            onClick={() => setCategoryId(i)}
+                            className={categoryId == i ? style.active : ''}
+                            key={obj.name}>{obj.name}
+                        </li>
+                    ))}
                 </ul>
-                <input className={style.search_input} placeholder="Поиск по названию" />
+                <input
+                    value={searchValue}
+                    onChange={e => setSearchValue(e.target.value)}
+                    className={style.search_input}
+                    placeholder="Search by title" />
             </div>
             <div className={style.content}>
-                {collections.map((obj: any, index) => (
-                    <Collection key={index} name={obj.name} images={obj.photos}/>
-                ))}
+                {isLouding ? (
+                    <h2>Data is louding ...</h2>
+                ) : (
+                    collections.filter((obj: any) => obj.name.toLowerCase().includes(searchValue.toLocaleLowerCase()))
+                        .map((obj: any, index) => (
+                            <Collection key={index} name={obj.name} images={obj.photos} />))
+                )}
 
             </div>
             <ul className={style.pagination}>
-                <li>1</li>
-                <li className={style.active}>2</li>
-                <li>3</li>
+                {[...Array(5)].map((_, i) => (
+                        <li onClick={() => setPage(i + 1)} className={page == i + 1 ? style.active : ''}>{i + 1}</li>
+                    ))}
             </ul>
         </div>
     );
--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -4,11 +4,11 @@ import style from './Collection.modules.scss'
 const Collection: React.FC<any> = ({images, name}) => {
     return (
         <div className={style.collection}>
-            <img className={style.__big} src={images[0]} alt="Item" />
-            <div className={style.__bottom}>
-                <img className={style.__mini} src={images[1]} alt="Item" />
-                <img className={style.__mini} src={images[2]} alt="Item" />
-                <img className={style.__mini} src={images[3]} alt="Item" />
+            <img className={style.collection__big} src={images[0]} alt="Item" />
+            <div className={style.collection__bottom}>
+                <img className={style.collection__mini} src={images[1]} alt="Item" />
+                <img className={style.collection__mini} src={images[2]} alt="Item" />
+                <img className={style.collection__mini} src={images[3]} alt="Item" />
             </div>
             <h4>{name}</h4>
         </div>
